aa29d2ae1f1deded53be17909634448aab6cecf4
Jhon Quitian <quitian.j2010@gmail.com>
2026-06-11 14:34:24 -0500

    fin sección 6

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,7 @@
 import { defineCollection } from "astro/content/config";
 import { glob } from "astro/loaders";
 import { z } from "astro/zod";
+import { reference } from "astro:content";
 
 const blogCollection = defineCollection({
     loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
@@ -12,13 +13,32 @@ const blogCollection = defineCollection({
         image: image(),
 
         // relación
-        author: z.string(),
+        // author: z.string(),
+        author: reference('author'),
 
         // Relación
         tags: z.array(z.string()),
+
+        // Boolean
+        isDraft: z.boolean().default(false),
     })
 });
 
+const authorCollection = defineCollection({
+    loader: glob({ base: './src/content/author', pattern: '**/*.{yml,yaml}' }),
+    // type: 'data',
+    schema: ({ image }) => z.object({
+        name: z.string(),
+        avatar: image(),
+        twitter: z.string(),
+        linkedIn: z.string(),
+        github: z.string(),
+        bio: z.string(),
+        subtitle: z.string(),
+    })
+})
+
 export const collections = {
-    blog: blogCollection
+    blog: blogCollection,
+    author: authorCollection,
 };
